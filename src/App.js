dc551220f550c26cb625b750899c66ac0ed58203
ThePandaJam <veronika.kravchenko@barbal.co>
2021-08-30 20:53:07 +0100

    prepared app for deployment

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ import axios from 'axios';
 
 const theme = createTheme(themeFile);
 
+axios.defaults.baseURL = 'https://europe-west1-test-forum-995a7.cloudfunctions.net/api';
+
 const token = localStorage.FBIdToken;
 if(token){
   const decodedToken = jwtDecode(token);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//firebase
+import { initializeApp } from "firebase/app";
+
+//firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBI8EqTVZZiYSQb_kdwbm60-Qzmp-8D_4c",
+  authDomain: "test-forum-995a7.firebaseapp.com",
+  projectId: "test-forum-995a7",
+  storageBucket: "test-forum-995a7.appspot.com",
+  messagingSenderId: "152503019495",
+  appId: "1:152503019495:web:2ca3c794ea56c423eaff42",
+  measurementId: "G-7RH8ZW42J4"
+};
+//initialise firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
