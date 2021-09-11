e10a9693ea1ac69940cd92a4e590714c925ffd76
ThePandaJam <veronika.kravchenko@barbal.co>
2021-09-11 22:56:58 +0100

    critical questions in dropdown. Selection sets argument text

--- src/components/post/CommentForm.js
+++ src/components/post/CommentForm.js
@@ -7,6 +7,11 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 
 //redux imports
@@ -17,7 +22,10 @@ import { Typography } from '@material-ui/core';
 
 const styles = (theme) => ({
     ...theme.loginSignupStyle,
-    
+    criticalQuestionDropdown: {
+        width: '100%',
+        margin: '10px auto 10px auto'
+    }
 })
 
 function CommentForm(props) {
@@ -30,6 +38,7 @@ function CommentForm(props) {
 
     const [errors, setErrors] = useState([])
     const [body, setBody] = useState("")
+    const [critQuestion, setCritQuestion] = useState("")
 
     useEffect(() => {
         if(uiErrors) {
@@ -47,6 +56,10 @@ function CommentForm(props) {
           setBody(value);
         }
       }
+    const dropdownHandler = (event) => {
+        setCritQuestion(event.target.value)
+        setBody(event.target.value)
+    }
     
       function handleSubmit(e) {
         e.preventDefault();
@@ -68,16 +81,30 @@ function CommentForm(props) {
                         </Grid>
                     ) : (
                         <Grid item sm={12} style={{alignContent:'center'}}>
-                        {schemeInfo.criticalQuestions.map((question) => (
-                                <Typography key={question.questionNo} variant="body1">{question.questionBody}</Typography>
-                            ))}
+                        
                         <form onSubmit={handleSubmit}>
+                            <FormControl className={classes.criticalQuestionDropdown}>
+                                <InputLabel>Critical Question</InputLabel>
+                                <Select
+                                name="critQuestion"
+                                id="demo-simple-select"
+                                value={critQuestion}
+                                onChange={dropdownHandler} 
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                {schemeInfo.criticalQuestions.map((question) => (
+                                    <MenuItem key={question.questionNo} value={question.questionBody}>{question.questionBody}</MenuItem>
+                                ))}
+                                </Select>
+                            </FormControl>
                             <TextField
                                 name="body"
                                 type="text"
                                 label="Add an argument"
                                 error={errors.comment ? true : false}
-                                helperText={errors.body}
+                                helperText={errors.comment}
                                 value={body} 
                                 onChange = {(event) => onChangeHandler(event)} 
                                 fullWidth
