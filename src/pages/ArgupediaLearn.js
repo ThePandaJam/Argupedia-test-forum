c3cc3ff34e711324bccb335389f47d2c248d8067
ThePandaJam <veronika.kravchenko@barbal.co>
2021-09-14 17:54:13 +0100

    added topic cards

--- src/pages/ArgupediaLearn.js
+++ src/pages/ArgupediaLearn.js
@@ -10,6 +10,7 @@ import Grid from '@material-ui/core/Grid'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import { Card, CardContent, Paper } from '@material-ui/core';
 
 const styles = (theme) => ({
     ...theme.loginSignupStyle,
@@ -30,7 +31,7 @@ const styles = (theme) => ({
         width: '100%',
     },
     createPostForm: {
-        textAlign: 'center',
+        textAlign: 'left',
         padding: 25
     },
 })
@@ -65,36 +66,86 @@ function ArgupediaLearn(props) {
 
     return (
         <Fragment>
-            <Grid container spacing={4}>
-                <Grid item sm={4} xs={12} className={classes.tabSelector}>
-                <Tabs
-                    orientation="vertical"
-                    variant="fullWidth"
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="Tab selection"
-                    className={classes.tabs}
-                >
-                    <Tab label="Schemes and Critical Questions"/>
-                    <Tab label="Argument Graphs and Labelling"/>
-                </Tabs>
-                
-                </Grid>
-                <Grid item sm={8} xs={12} className={classes.createPostForm}>
-                    <TabPanel value={value} index={0}>
-                        <Typography variant="h4" className={classes.pageTitle}>
-                            Schemes and Critical Questions
+            <Tabs
+                //orientation="vertical"
+                variant="fullWidth"
+                value={value}
+                onChange={handleChange}
+                aria-label="Tab selection"
+                className={classes.tabSelector}
+            >
+                <Tab label="Schemes and Critical Questions"/>
+                <Tab label="Argument Graphs and Labelling"/>
+            </Tabs>
+
+            <TabPanel value={value} index={0}>
+                <Typography variant="h4" className={classes.pageTitle}>
+                    Schemes and Critical Questions
+                </Typography>
+                <hr className={classes.visibleSeparator}/>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            What are Schemes?
                         </Typography>
-                        <hr className={classes.visibleSeparator}/>
-                    </TabPanel>
-                    <TabPanel value={value} index={1}>
-                        <Typography variant="h4" className={classes.pageTitle}>
-                            Argument graphs and Labelling
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            What are Critical Questions?
                         </Typography>
-                        <hr className={classes.visibleSeparator}/>
-                    </TabPanel>
-                </Grid>
-            </Grid>
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            How can I start a debate?
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            How do I construct an argument?
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            How do I respond to an argument?
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <Typography variant="h4" className={classes.pageTitle}>
+                    Argument graphs and Labelling
+                </Typography>
+                <hr className={classes.visibleSeparator}/>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            What are Argument graphs?
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            What are the different types of labels?
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5">
+                            How are labels calculated?
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </TabPanel>
         </Fragment> 
     )
 }
